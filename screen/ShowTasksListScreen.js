1e56c92bce52ac4ace51d74efb543616c3eb58bc
Chan <chan@iPod-Classic-128GB.local>
2023-10-16 16:47:59 +0100

    complete task completion function

--- screen/ShowTasksListScreen.js
+++ screen/ShowTasksListScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -27,12 +27,25 @@ const ShowTasksListScreen = () => {
   const [todoList, setTodoList] = useState([]);
   const [task, setTask] = useState("");
 
+  const taskCompletionHandler = async (i) => {
+    const newArr = [...todoList];
+    const completedTask = newArr[i];
+    completedTask["completed"] = true;
+    newArr.splice(i, 1);
+    newArr.push(completedTask);
+    setTodoList(newArr);
+    await storeData();
+  };
+
   const taskAdditionHandler = async () => {
     const newTask = { task: task, completed: false };
     const newArr = [...todoList];
     newArr.push(newTask);
     setTodoList(newArr);
+    await storeData();
+  };
 
+  const storeData = async () => {
     try {
       const jsonValue = JSON.stringify(todoList);
       await AsyncStorage.setItem("to-do-list", jsonValue);
@@ -52,7 +65,18 @@ const ShowTasksListScreen = () => {
         text={"Add task"}
       />
       {todoList.map((obj, i) => {
-        return <Text key={i}>{obj.task}</Text>;
+        return (
+          <TouchableOpacity
+            key={i}
+            onPress={() => {
+              taskCompletionHandler(i);
+            }}
+          >
+            <Text>
+              {obj.task} {obj.completed ? "completed" : "incompleted"}
+            </Text>
+          </TouchableOpacity>
+        );
       })}
     </View>
   );
